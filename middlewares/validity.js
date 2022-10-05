3fba2af0d31c635c68c832503b880c7c6afc87bb
Ruslan Fedin <fedin.ra@mail.ru>
2022-10-05 18:24:14 +0300

    added Joi body: name: requred

--- middlewares/validity.js
+++ middlewares/validity.js
@@ -19,7 +19,7 @@ const signUpValidity = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8).max(30),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
   }),
 });
 
